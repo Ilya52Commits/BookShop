90259c1f4ee92cbc46ac61b5ea59c8c557bc3e85
Ilya Krass <krasnenkov.ilya@inbox.ru>
2024-04-16 15:30:44 +0400

    Добавьте файлы проекта.

---
+++ DbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookShopCore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopCore;
+internal class DbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+    optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=booksDB;Username=postgres;Password=52");
+
+  public DbSet<User> Users { get; set; }
+  public DbSet<Product> Products { get; set; }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShopCore.Model;
+
+public class User
+{
+  public int Id { get; set; }
+  public string Login { get; set; }
+  public string Password { get; set; }
+  public string Type { get; set; }
+}
+
+public class Product
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string Author { get; set; }
+
+  public string Genre { get; set; }
+
+  public string Price { get; set; }
+}
