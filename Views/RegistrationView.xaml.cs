93f16356ddeaed6eb2b0652eb071d65db045b858
Ilya Krass <krasnenkov.ilya@inbox.ru>
2024-04-17 15:04:26 +0400

    Сделал дизайн RegistrationView; добавил начальную логику для проверки вводимых данных; создал ClientProductView

--- Views/RegistrationView.xaml.cs
+++ Views/RegistrationView.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BookShopCore.Views
 {
@@ -24,5 +14,63 @@ namespace BookShopCore.Views
     {
       InitializeComponent();
     }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var loginText = Login.Text.Trim();
+      var emailText = Email.Text.Trim();
+      var passwordText = Password.Password.Trim();
+      var confPasswordText = ConfPassword.Password.Trim();
+
+      
+    }
+
+    private static bool IsNumberAlreadyRegistred(string str)
+    {
+      
+
+      return false;
+    }
+
+    public static bool IsPhoneCorrect(string str)
+    {
+      if (str.Length != 11 || str.Length != 12)
+        return false;
+
+      return true;
+    }
+
+    // function of chek login and password 
+    private static void CheckLoginPassword()
+    {
+      
+    }
+
+    private static bool CheckEmail(string email)
+    {
+      if (!email.Contains("@"))
+        return false;
+
+      string[] delimiter = email.Split('@');
+      if (delimiter.Length != 2)
+        return false;
+
+      string login = delimiter[0];
+      string domain = delimiter[1];
+
+      if (login.Length == 0 || domain.Length == 0)
+        return false;
+
+      char firstSymbol = login[0];
+      if (Regex.IsMatch(firstSymbol.ToString(), @"[A-Z][a-z]"))
+        return false;
+
+      string[] arrDobain = domain.Split('.');
+      if (arrDobain.Length != 2)
+        return false;
+
+      return true;
+    }
+
   }
 }
