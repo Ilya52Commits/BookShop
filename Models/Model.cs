d12921cee30b376cd3a44e55dc7b0af821986af6
Ilya <krasnenkov.ilya@inbox.ru>
2024-05-16 13:49:36 +0400

    Корректирование структуры xaml файлов регистрации и авторизации (удаление border)

--- Models/Model.cs
+++ Models/Model.cs
@@ -4,21 +4,21 @@
 /* Модель таблицы пользователя */
 public class User
 {
-  public int Id { get; init; }                                  // Идентификатор пользователя
-  public string Login { get; init; }                            // Логин пользователя
-  public string Email { get; init; }                            // Почта пользователя
-  public string Password { get; init; }                         // Пароль ползователя
-  public string Role { get; init; }                             // Тип пользователя
-  public ICollection<Book> SelectedBooks { get; set; } = [];    // Коллекция выбранных книг
+  public int Id { get; init; }                                // Идентификатор пользователя
+  public string? Login { get; init; }                         // Логин пользователя
+  public string? Email { get; init; }                         // Почта пользователя
+  public string? Password { get; init; }                      // Пароль ползователя
+  public string? Role { get; init; }                          // Тип пользователя
+  public ICollection<Book> SelectedBooks { get; init; } = []; // Коллекция выбранных книг
 }
 
 /* Модель таблицы книг */ 
 public class Book
 {
-  public int Id { get; set; }           // Идентификатор книги
-  public string Name { get; set; }      // Название книги
-  public string Author { get; set; }    // Автор продукта
-  public string Genre { get; set; }     // Жанр книги
-  public int Price { get; set; }        // Цена книги
+  public int Id { get; init; }        // Идентификатор книги
+  public string? Name { get; set; }   // Название книги
+  public string? Author { get; set; } // Автор продукта
+  public string? Genre { get; set; }  // Жанр книги
+  public int Price { get; set; }      // Цена книги
 }
 #endregion
